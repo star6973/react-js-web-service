dd1430ed3b8b564bb19c93ff2db256b0063aa0ab
mhji <battlesun99@gmail.com>
2021-07-26 16:57:04 +0900

    Feat: jsx + props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import React from "react";
+
+function Food({favorite1, favorite2}) { // props를 인자로 받으면, Object 안에 favorite이라는 변수가 있음. 왼쪽 같이 표현하는 것은 ES6 문법임.
+  return (
+    <div>
+      <h1>I love {favorite1}</h1>
+      <h2>I love {favorite2}</h2>
+    </div>
+  )
+}
+
+// component -> component로 정보를 보내는 방법
+// Food component에 favorite이라는 이름의 property를 potato라는 value로 전달
+// 누군가가 Food component에 정보를 보내려고 하면, react는 밑에 보이는 속성들을 가져올 것임
+// 그 속성들은 Food function component의 argument로 들어가짐
+// jsx + props를 사용하여 재사용성을 높인다
 function App() {
   return (
-    <h1>Hello</h1>
+    <div>
+      <h1>Hello</h1>
+      <Food favorite1="tomato" />
+      <Food favorite2="potato" />
+      <Food favorite3="samgiopsal" />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 // root 태그에 App virtual Dom을 렌더링함
+// <App /> <-- Component라고 불림, HTML을 반환하는 함수
+// HTML과 Javascript간의 이러한 구조를 jsx라고 불림
+// React는 component로 동작함
 ReactDOM.render(
   <React.StrictMode>
     <App />
